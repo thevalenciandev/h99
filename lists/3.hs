f674164107219676835e1499ecbb1b3902114f25
TVD <thevalenciandev@gmail.com>
2021-06-14 18:45:23 +0100

    3. Find the Kth element of a list. The first element in the list is number 1: Refactor

--- lists/3.hs
+++ lists/3.hs
@@ -1,4 +1,4 @@
 elemAt :: Int -> [a] -> a
 elemAt k
     | k <= 0    = error "Index too low"
-    | otherwise = snd . head . dropWhile (\(i,_) -> i/=k) . zip [1..]
+    | otherwise = head . drop (k-1)
