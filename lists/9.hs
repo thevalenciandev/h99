52a2fec94ac8420645a0449ac1d4a5d662ab6c2d
TVD <thevalenciandev@gmail.com>
2021-06-16 06:51:24 +0100

    9. Pack consecutive duplicates of list elements into sublists. More solutions

--- lists/9.hs
+++ lists/9.hs
@@ -2,3 +2,8 @@ import Data.List
 
 pack :: Eq a => [a] -> [[a]]
 pack = group
+
+pack' :: Eq a => [a] -> [[a]]
+pack' [] = []
+pack' (x:xs) = let (ys,zs) = span (==x) xs
+               in  (x:ys) : pack' zs
